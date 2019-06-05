d081f3778503ab886a2120ec3304642d945d1895
mantas <mantas.petrauskas96@gmail.com>
2019-06-05 16:42:29 +0300

    remove redundant comment

--- src/Templates/Vue/DummyTable.spec.js
+++ src/Templates/Vue/DummyTable.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils';
 import DummyTable from '../../src/components/DummyTable';
-import { dummys } from './mock/dummys';
+import { mockDummys } from './mock/dummys';
 
 describe('DummyTable', () => {
   let wrapper;
@@ -8,7 +8,7 @@ describe('DummyTable', () => {
   beforeEach(() => {
     wrapper = mount(DummyTable, {
       propsData: {
-        rows: dummys,
+        rows: mockDummys,
         pagination: {
           totalItems: 100,
           rowsPerPage: 50,
@@ -19,13 +19,12 @@ describe('DummyTable', () => {
   });
 
   it('renders a row for each item in props.rows', () => {
-    // * 2 because each content row has en expand row. +2 because of header row and progress bar row
-    expect(wrapper.findAll('.table-row')).toHaveLength(dummys.length);
+    expect(wrapper.findAll('.table-row')).toHaveLength(mockDummys.length);
   });
 
   it('emits a rowClick event with dummy as payload when a row is clicked', () => {
     wrapper.find('.table-row').trigger('click');
-    expect(wrapper.emitted().rowClick[0]).toEqual([dummys[0]]);
+    expect(wrapper.emitted().rowClick[0]).toEqual([mockDummys[0]]);
   });
 
   it('emits a changePage event with page number as payload when next page button is clicked', () => {
@@ -35,7 +34,7 @@ describe('DummyTable', () => {
 
   it('emits a delete event with dummy as payload when delete button is clicked', () => {
     wrapper.findAll('.row-actions button').at(0).trigger('click');
-    expect(wrapper.emitted().delete[0]).toEqual([dummys[0]]);
+    expect(wrapper.emitted().delete[0]).toEqual([mockDummys[0]]);
   });
 
   it('matches snapshot', () => {
